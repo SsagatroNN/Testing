3952658cb99a6867d83d9315625b4f56ed4aa736
yi <yz7420@ic.ac.uk>
2022-12-02 11:56:59 +0000

    modified

--- RISC-V-Reduced/alu.sv
+++ RISC-V-Reduced/alu.sv
@@ -20,7 +20,7 @@ module alu #(
       3'b010:  aluout = aluop1 & aluop2;
       3'b011:  aluout = aluop1 | aluop2;
       3'b100:  aluout = 32'b0;
-askjdhdjaskdhkjashd
+askjdhdja
       3'b110:  aluout = 32'b0;
       3'b111:  aluout = 32'b0;
       default: aluout = 0;
@@ -41,8 +41,4 @@ asf]asf
 <MAX; ++asfd
 asfd
 asf
-asf]asf
-) begin
-  
-end
-endmodule
+
